ea48fe626917ae4c5a31499283576c8e9227cb47
abdollah raafat <abdollah.raafat.daif@gmail.com>
2022-05-19 20:21:02 +0200

    Add empty nav have logo and h1

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
+import '../styles/App.css';
+import NavBar from "./NavBar.js";
 
 function App() {
 
   return (
     <div className="App">
+      <NavBar />
     </div>
   );
 }
