1e72d3a6075e6e29d4a46c2dc494d564c5e696b2
lyamz <davidlyangenda@gmail.com>
2022-05-20 11:16:01 +0200

    Add three routers

---
+++ src/Components/Nav.js
@@ -0,0 +1,21 @@
+import '../styles/navbar.css';
+import { Link } from 'react-router-dom';
+import logo from '../logo.png'
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div>
+      <img id="logo" src={logo} alt="Logo" />
+      <h1>Space hub</h1>
+      </div>
+      <div className='links'>
+        <Link to='/'>Rockets</Link>
+        <Link to='/Mission'>Mission</Link>
+        <Link to='/MyProfile'>MyProfile</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
